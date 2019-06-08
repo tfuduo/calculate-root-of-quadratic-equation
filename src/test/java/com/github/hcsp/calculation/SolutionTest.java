e52c02cf6567e3f5ea647ef328db952f2bd8967b
Bo Zhang <bo@gradle.com>
2019-06-08 17:53:45 +0800

    Init repo

---
+++ src/test/java/com/github/hcsp/calculation/SolutionTest.java
@@ -0,0 +1,24 @@
+package com.github.hcsp.calculation;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+    @Test
+    public void test() {
+        assertDoubleArrayEquals(new double[] {2d}, Solution.calculate(1, -4, 4));
+        assertDoubleArrayEquals(new double[] {4d, -4d}, Solution.calculate(1, -16));
+        assertDoubleArrayEquals(new double[] {}, Solution.calculate(1, 1, 1));
+    }
+
+    private boolean assertDoubleArrayEquals(double[] expected, double[] actual) {
+        assertEquals(expected.length, actual.length);
+        Arrays.sort(expected);
+        Arrays.sort(actual);
+        for (int i = 0; i < expected.length; ++i) {
+            assertTrue(Math.abs(expected[i] - actual[i]) < 0.0001);
+        }
+    }
+}
