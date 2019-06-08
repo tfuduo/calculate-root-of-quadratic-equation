91826e7f2a76c027488fa6e503afd6186ff38701
Bo Zhang <bo@gradle.com>
2019-06-08 19:48:39 +0800

    Update repo

--- src/test/java/com/github/hcsp/calculation/SolutionTest.java
+++ src/test/java/com/github/hcsp/calculation/SolutionTest.java
@@ -9,14 +9,14 @@ class SolutionTest {
     @Test
     public void test() {
         assertDoubleArrayEquals(new double[] {2d}, Solution.calculate(1, -4, 4));
-        assertDoubleArrayEquals(new double[] {4d, -4d}, Solution.calculate(1, -16));
+        assertDoubleArrayEquals(new double[] {4d, -4d}, Solution.calculate(1, 0, -16));
         assertDoubleArrayEquals(new double[] {}, Solution.calculate(1, 1, 1));
     }
 
-    private boolean assertDoubleArrayEquals(double[] expected, double[] actual) {
+    private void assertDoubleArrayEquals(double[] expected, double[] actual) {
         assertEquals(expected.length, actual.length);
-        Arrays.sort(expected);
-        Arrays.sort(actual);
+        java.util.Arrays.sort(expected);
+        java.util.Arrays.sort(actual);
         for (int i = 0; i < expected.length; ++i) {
             assertTrue(Math.abs(expected[i] - actual[i]) < 0.0001);
         }
