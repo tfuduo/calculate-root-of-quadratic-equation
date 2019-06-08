ba58866a596bd353cea7f19b503d14bde40cb80e
Bo Zhang <bo@gradle.com>
2019-06-08 17:54:54 +0800

    Init repo

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -15,5 +15,5 @@ public class Solution {
     // 若没有根，返回一个空数组，即：new double[] {}
     // 提示，你可利用求根公式x=[-b±√(b²-4ac)]/2a
     // 需要执行开方运算时可使用Math.sqrt()方法
-    public double[] calculate(int a, int b, int c) {}
+    public static double[] calculate(int a, int b, int c) {}
 }
