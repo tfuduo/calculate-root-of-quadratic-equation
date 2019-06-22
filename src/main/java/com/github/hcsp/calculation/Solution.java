c8dc1c0375e168611abe013f0aca843d0cfad57f
Rokixy <48944401+Rokixy@users.noreply.github.com>
2019-06-22 16:39:29 +0800

    root (#17)
    root (#17)

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -15,5 +15,10 @@ public class Solution {
     // 若没有根，返回一个空数组，即：new double[] {}
     // 提示，你可利用求根公式x=[-b±√(b²-4ac)]/2a
     // 需要执行开方运算时可使用Math.sqrt()方法
-    public static double[] calculate(int a, int b, int c) {}
+    public static double[] calculate(int a, int b, int c) {
+
+        if(b*b - 4*a*c == 0) return new double[] {1.0*(-b)/2/a};
+        else if(b*b-4*a*c >0) return new double[] {1.0*(-b+Math.sqrt(b*b - 4*a*c))/2/a , 1.0*(-b-Math.sqrt(b*b - 4*a*c))/2/a};
+        return new double[] {};
+    }
 }
