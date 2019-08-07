3272abeffe9371cda8bdb8433b66bb4df3e3785f
Mingru Li <limingru1991@gmail.com>
2019-08-07 16:14:56 +0800

    求根 (#45)
    求根 (#45)

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -3,17 +3,28 @@ package com.github.hcsp.calculation;
 import java.util.Arrays;
 
 public class Solution {
-    public static void main(String[] args) {
-        System.out.println("x^2-2x+1=0的解是：" + Arrays.toString(calculate(1, -2, 1)));
-        System.out.println("x^2-4=0的解是：" + Arrays.toString(calculate(1, 0, -4)));
-        System.out.println("x^2+1=0的解是：" + Arrays.toString(calculate(1, 0, 1)));
-    }
+	public static void main(String[] args) {
+		System.out.println("x^2-2x+1=0的解是：" + Arrays.toString(calculate(1, -2, 1)));
+		System.out.println("x^2-4=0的解是：" + Arrays.toString(calculate(1, 0, -4)));
+		System.out.println("x^2+1=0的解是：" + Arrays.toString(calculate(1, 0, 1)));
+	}
 
-    // 计算一元二次方程ax^2+bx+c=0的根。
-    // 若有两个根，返回一个数组，包含这两个根，即：new double[] { root1, root2 }
-    // 若有一个根，返回一个数组，包含这个根，即：n返回new double[] { root }
-    // 若没有根，返回一个空数组，即：new double[] {}
-    // 提示，你可利用求根公式x=[-b±√(b²-4ac)]/2a
-    // 需要执行开方运算时可使用Math.sqrt()方法
-    public static double[] calculate(int a, int b, int c) {}
+	// 计算一元二次方程ax^2+bx+c=0的根。
+	// 若有两个根，返回一个数组，包含这两个根，即：new double[] { root1, root2 }
+	// 若有一个根，返回一个数组，包含这个根，即：n返回new double[] { root }
+	// 若没有根，返回一个空数组，即：new double[] {}
+	// 提示，你可利用求根公式x=[-b±√(b²-4ac)]/2a
+	// 需要执行开方运算时可使用Math.sqrt()方法
+	public static double[] calculate(int a, int b, int c) {
+		double d = Math.sqrt(b * b - 4 * a * c);
+		double one = (-b + d) / 2 * a;
+		double two = (-b - d) / 2 * a;
+		if (Double.isNaN(one)) {
+			return new double[]{};
+		}
+		if (Math.abs(one - two) < 0.00000001) {
+			return new double[]{one};
+		}
+		return new double[]{one, two};
+	}
 }
