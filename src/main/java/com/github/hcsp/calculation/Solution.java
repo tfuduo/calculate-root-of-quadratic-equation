95c514196b896654d5f5fbc61e93ec81eef4c3d1
YakumoAoi <416910533@qq.com>
2019-08-08 15:09:23 +0900

    calculate_square_root (#46)
    calculate_square_root (#46)

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -15,5 +15,19 @@ public class Solution {
     // 若没有根，返回一个空数组，即：new double[] {}
     // 提示，你可利用求根公式x=[-b±√(b²-4ac)]/2a
     // 需要执行开方运算时可使用Math.sqrt()方法
-    public static double[] calculate(int a, int b, int c) {}
+    public static double[] calculate(int a, int b, int c) {
+        double x;
+        double y;
+        int beforeSqrt=b*b-4*a*c;
+        if(beforeSqrt==0){
+            x=-b/(2*a);
+            return new double[]{x};
+        }
+        if(beforeSqrt<0){
+            return new double[]{};
+        }
+        x=(-b+Math.sqrt(beforeSqrt))/(2*a);
+        y=(-b-Math.sqrt(beforeSqrt))/(2*a);
+        return new double[]{x,y};
+    }
 }
