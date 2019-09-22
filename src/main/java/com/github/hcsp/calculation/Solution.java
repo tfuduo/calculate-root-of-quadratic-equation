5156687e7a8c08e8f685c59b42e133d884549564
xym11 <xymreasonxxs@163.com>
2019-09-23 03:23:17 +0800

    解一元二次方程

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -15,5 +15,21 @@ public class Solution {
     // 若没有根，返回一个空数组，即：new double[] {}
     // 提示，你可利用求根公式x=[-b±√(b²-4ac)]/2a
     // 需要执行开方运算时可使用Math.sqrt()方法
-    public static double[] calculate(int a, int b, int c) {}
+    public static double[] calculate(int a, int b, int c) {
+        double m = b * b - 4 * a * c;
+        if (a == 0 && b != 0){
+            return new double[] {-c / b};
+        }
+        if (a == 0 && b == 0) {
+            return new double[] {};
+        }
+        if (m < 0){
+            return new double[]{};
+        }
+        if (m == 0){
+            return new double[] { -b / (2 * a)};
+        }
+        return new double[] {(-b + Math.sqrt(m)) / (2 * a), (-b - Math.sqrt(m)) / (2 * a)};
+
+    }
 }
