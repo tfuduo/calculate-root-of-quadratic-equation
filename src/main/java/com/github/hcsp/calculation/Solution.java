f18d54940aa9547ba0da25d8480eca7747ac5c1b
ws1603 <48832455+ws1603@users.noreply.github.com>
2019-10-10 16:46:08 +0800

    求一元二次方程的根 (#72)
    求一元二次方程的根 (#72)

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -15,5 +15,17 @@ public class Solution {
     // 若没有根，返回一个空数组，即：new double[] {}
     // 提示，你可利用求根公式x=[-b±√(b²-4ac)]/2a
     // 需要执行开方运算时可使用Math.sqrt()方法
-    public static double[] calculate(int a, int b, int c) {}
+    public static double[] calculate(int a, int b, int c) {
+        double a1 = b * b - 4 * a * c;
+        if (a1 > 0) {
+            double root1 = (-b + Math.sqrt(a1)) / 2 * a, root2 = (-b - Math.sqrt(a1)) / 2 * a;
+            return new double[]{root1, root2};
+        }
+        if (a1 == 0) {
+            double root = -b / 2 * a;
+            return new double[]{root};
+        }
+        return new double[]{};
+    }
 }
+
