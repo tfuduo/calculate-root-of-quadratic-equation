4c7765fe7884ed78a36cdf79492fc5817ce63375
emm <3108775859@qq.com>
2019-10-12 19:00:01 +0800

    数学比程序难吗

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -15,5 +15,17 @@ public class Solution {
     // 若没有根，返回一个空数组，即：new double[] {}
     // 提示，你可利用求根公式x=[-b±√(b²-4ac)]/2a
     // 需要执行开方运算时可使用Math.sqrt()方法
-    public static double[] calculate(int a, int b, int c) {}
+    public static double[] calculate(int a, int b, int c) {
+        double discriminant = Math.pow(b,2) - 4*a*c;
+        if (discriminant > 0){
+            double root1 = (-b + Math.sqrt(discriminant))/2/a;
+            double root2 = (-b - Math.sqrt(discriminant))/2/a;
+            return new double[]{root1,root2};
+        }else if(discriminant == 0){
+            double root = (-b + Math.sqrt(discriminant))/2/a;
+            return new double[]{root};
+        }else
+            return new double[]{};
+
+    }
 }
