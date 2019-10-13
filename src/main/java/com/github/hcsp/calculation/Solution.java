ef3639fea90a64afdb673d0a18eae7a85460f036
dqprecisian <1352034864@qq.com>
2019-10-13 20:57:24 +0800

    计算一元二次方程

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -19,8 +19,13 @@ public class Solution {
         double d = b * b - 4 * a * c;
         if(d == 0) {
             return new double[]{-b / 2 / a};
-        } else {
-            return new double[]{(-b + Math.sqrt(d)) / 2 / a, (-b - Math.sqrt(d)) / 2 / a};
         }
+
+        if(d < 0) {
+            return new double[]{};
+        }
+
+        return new double[]{(-b + Math.sqrt(d)) / 2 / a, (-b - Math.sqrt(d)) / 2 / a};
+
     }
 }
