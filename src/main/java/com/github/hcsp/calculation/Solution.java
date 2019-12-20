9ce39040afce0fef97c2983219db9123236bdb19
NervousOrange <zch693922442@outlook.com>
2019-12-20 13:21:49 +0100

    from NervousOrange: Assignment 2 of basic calculate symbol.

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -15,5 +15,20 @@ public class Solution {
     // 若没有根，返回一个空数组，即：new double[] {}
     // 提示，你可利用求根公式x=[-b±√(b²-4ac)]/2a
     // 需要执行开方运算时可使用Math.sqrt()方法
-    public static double[] calculate(int a, int b, int c) {}
+    public static double[] calculate(int a, int b, int c) {
+        double sqrt = Math.sqrt(b * b - 4 * a * c);
+        if (a == 0) {
+            double x = (-c / b) * 1.0;
+            return new double[]{x};
+        } else if (sqrt > 0) {
+            double x1 = (-b + sqrt) / (2 * a);
+            double x2 = (-b - sqrt) / (2 * a);
+            return new double[]{x1, x2};
+        } else if (sqrt == 0) {
+            return new double[]{ (-b + sqrt) / (2 * a)};
+        }
+        else {
+            return new double[]{};
+        }
+    }
 }
