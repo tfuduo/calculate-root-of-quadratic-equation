539c53012dda607bd60d8b1eb77e4f469b410e7b
zml59 <59412107+zml59@users.noreply.github.com>
2020-01-07 23:56:27 +0800

    求一元二次方程，去掉if语句中的大括号。 (#129)
    求一元二次方程，去掉if语句中的大括号。 (#129)

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -15,5 +15,16 @@ public class Solution {
     // 若没有根，返回一个空数组，即：new double[] {}
     // 提示，你可利用求根公式x=[-b±√(b²-4ac)]/2a
     // 需要执行开方运算时可使用Math.sqrt()方法
-    public static double[] calculate(int a, int b, int c) {}
+    public static double[] calculate(int a, int b, int c) {
+        int m = b * b - 4 * a * c;
+        double v1 = (-b + Math.sqrt(m)) / 2 * a;
+        double v2 = (-b - Math.sqrt(m)) / 2 * a;
+        if (m >= 0)
+            if (Math.abs(v1 - v2)< 1e-6)
+                return new double[]{v1};
+            else
+                return new double[]{v1, v2};
+        else
+            return new double[]{};
+    }
 }
