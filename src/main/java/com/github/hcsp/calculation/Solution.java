ba2d15fa41ffac7bdb9d78b8ca220c873e7e52f4
Airtoit <51944383+Airtoit@users.noreply.github.com>
2020-02-28 17:48:57 +0800

    calculate方法实现计算一元二次方程的根 (#150)
    calculate方法实现计算一元二次方程的根 (#150)

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -15,5 +15,17 @@ public class Solution {
     // 若没有根，返回一个空数组，即：new double[] {}
     // 提示，你可利用求根公式x=[-b±√(b²-4ac)]/2a
     // 需要执行开方运算时可使用Math.sqrt()方法
-    public static double[] calculate(int a, int b, int c) {}
+    public static double[] calculate(int a, int b, int c) {
+        int discriminant = b * b - 4 * a * c;
+        if (discriminant < 0) {
+            return new double[] {};
+        }
+        if (discriminant == 0) {
+            return new double[] {(-1.0) * b / (a * 2)};
+        }
+        double tmp = Math.sqrt(discriminant);
+        return new double[] {((-1.0) * b + tmp) / (a * 2), ((-1.0) * b - tmp) / (a * 2)};
+
+
+    }
 }
