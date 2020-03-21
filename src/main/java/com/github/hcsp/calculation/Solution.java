9c5d0044a83c26dc3a79d13c38c817c03dc1b183
Huang xuewei <770342643@qq.com>
2020-03-22 04:32:13 +0800

    add calculation root of quadratic equation codes in calculate method. (#171)
    add calculation root of quadratic equation codes in calculate method. (#171)

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -15,5 +15,14 @@ public class Solution {
     // 若没有根，返回一个空数组，即：new double[] {}
     // 提示，你可利用求根公式x=[-b±√(b²-4ac)]/2a
     // 需要执行开方运算时可使用Math.sqrt()方法
-    public static double[] calculate(int a, int b, int c) {}
+    public static double[] calculate(int a, int b, int c) {
+        int formula = b * b - 4 * a * c;
+
+    return formula < 0
+        ? new double[] {}
+        : formula == 0
+            ? new double[] {-b / 2 * a}
+            : new double[] {(-b + Math.sqrt(formula) / 2 * a), (-b - Math.sqrt(formula) / 2 * a)};
+    }
+
 }
